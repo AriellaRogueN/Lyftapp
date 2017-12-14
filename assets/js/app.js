6c43f7089cfc685d8d2817682bb7b1b66379a3ea
AriellaRogueN <arrodriguezn@gmail.com>
2017-12-14 19:32:08 -0300

    agregando cambios a margenes y formularios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,64 @@
 $(document).ready(function () {
-    /*Funcion al hacer click en el primer boton redireccione a la vista 2*/
-    $("#sign-up").click(function () {
-       window.location = 'index2.html' 
+  /*Función al hacer click en el primer botón redireccione a la vista 2*/
+  $("#sign-up").click(function () {
+    window.location = 'index2.html'
+  });
+  /*Función para validar los digitos ingresados por el usuario, esto permite habilitar el boton*/
+  $("#tel").keydown(function () {
+    var phone = $(this).val();
+    for (var i = 0; i < 1; i++) {
+      if (phone.length == 8) {
+        $("#in-btn").removeAttr("disabled")
+      } else {
+        $("#in-btn").attr("disabled", "disabled")
+      };
+    };
+  });
+
+  $("#in-btn").click(function () {
+    window.location = 'index3.html'
+  });
+
+  /*Función que genera numeros de forma aleatoria*/
+  $("#in-btn").click(function () {
+    var code = "";
+    var str = "123456789";
+    for (var i = 0; i < 3; i++) {
+      code += str.charAt(Math.floor(Math.random() * str.length));
+    };
+    alert("El código es LAB " + code);
+  });
+  /*Validar código*/
+  $("#yourcode").keydown(function () {
+    var code = $(this).val();
+    for (var i = 0; i < 1; i++) {
+      if (code.length == 2) {
+        $("#resend-btn").removeAttr("disabled")
+      } else {
+        $("#resend-btn").attr("disabled", "disabled")
+      };
+    };
+  });
+
+  $("#next-btn").click(function () {
+    window.location = 'index4.html'
+
+  });
+  /*Habilitar botón next de vista 4*/
+  $("#yourcode").keydown(function () {
+    var code = $(this).val();
+    for (var i = 0; i < 1; i++) {
+      if (code.length == 2) {
+        $("#next-btn").removeAttr("disabled")
+      } else {
+        $("#next-btn").attr("disabled", "disabled")
+      };
+    };
+  });
+
+  $("#to-btn").click(function () {
+    window.location = 'index5.html'
+
+  });
+
 });
-    /*Funcion para validar los digitos ingresados por el usuario, esto permite habilitar el boton*/
-    $("#tel").keydown(function () {
-        var phone = $(this).val();
-        for (var i = 0; i < 1; i++) {
-            if (phone.length == 9) {
-                $("#in-btn").removeAttr("disabled")
-            } else {
-                $("#in-btn").attr("disabled", "disabled")
-            };
-        };
-    });
-
-    $("#in-btn").click(function () {
-        window.location = 'index3.html'
-    });
-
-     /*Funcion que genera numeros de forma aleatoria*/   
-    $("#in-btn").click(function () {
-        var code = "";
-        var str = "123456789";
-        for (var i = 0; i < 3; i++) {
-            code += str.charAt(Math.floor(Math.random() * str.length));
-        };
-        alert("El código es LAB " + code);
-    });
-     /*Validar codigo*/
-    $("#yourcode").keydown(function () {
-        var code = $(this).val();
-        for (var i = 0; i < 1; i++) {
-            if (code.length == 3) {
-                $("#resend-btn").removeAttr("disabled")
-            } else {
-                $("#resend-btn").attr("disabled", "disabled")
-            };
-        };
-    });
-
-    $("#next-btn").click(function () {
-        window.location = 'index4.html'
-
-    });
-    /*Habilitar boton next de vista 4*/
-    $("#yourcode").keydown(function () {
-        var code = $(this).val();
-        for (var i = 0; i < 1; i++) {
-            if (code.length == 3) {
-                $("#next-btn").removeAttr("disabled")
-            } else {
-                $("#next-btn").attr("disabled", "disabled")
-            };
-        };
-    });
- 
-    $("#to-btn").click(function () {
-        window.location = 'index5.html'
-
-    });
-       
-});
-
