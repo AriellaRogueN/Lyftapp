bee633032e52b9b2066de644e5596a5c615e112e
AriellaRogueN <arrodriguezn@gmail.com>
2017-12-14 09:03:23 -0300

    funcionalidad a botones y generacion de codigo aleatorio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
-    $("#log-in").click(function () {
-
-        $("#log-in, #sign-up").hide();
+    $("#sign-up").click(function () {
+       window.location = 'index2.html' 
+});
+    $("#tel").keydown(function () {
+        var phone = $(this).val();
+        for (var i = 0; i < 1; i++) {
+            if (phone.length == 9) {
+                $("#in-btn").removeAttr("disabled")
+            } else {
+                $("#in-btn").attr("disabled", "disabled")
+            };
+        };
+    });
         
-        $(".container").show();
-        
-
+    $("#in-btn").click(function () {
+        var code = "";
+        var str = "123456789";
+        for (var i = 0; i < 3; i++) {
+            code += str.charAt(Math.floor(Math.random() * str.length));
+        };
+        alert("El código es LAB " + code);
     });
 
+    $("#in-btn").click(function () {
+        window.location = 'index3.html'
     });
-
+    
+    
+       
+});
 
